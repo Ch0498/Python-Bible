c94886ff0acba084d19edf8fa4c3b55d10cf8d83
Navdeep Kante <65970997+smartnavdeep@users.noreply.github.com>
2020-06-21 20:57:22 +0530

    Update health.py

--- health.py
+++ health.py
@@ -9,3 +9,6 @@ potion_health = int(random.randint(25,50) / difficulty)
 health = health + potion_health
 
 print(health)
+while True:
+    health = int(input("Enter your player health"))
+    difficulty = int(input("Enter the difficulty of incresing the player health"))
