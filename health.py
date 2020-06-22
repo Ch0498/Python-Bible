1d00a83753bb27d06114336cd1300ad05261d5a9
Navdeep Kante <65970997+smartnavdeep@users.noreply.github.com>
2020-06-22 20:15:55 +0530

    Update health.py

--- health.py
+++ health.py
@@ -7,14 +7,10 @@ while True:
     difficulty = int(input("Enter the difficulty of incresing the player health [1, 2, 3]:"))
     sleep(0.5)
     print("...........Generating...........")
-    potion_health = int(randint(25,50))
+    potion_health = randint(25,50)
     new_health = potion_health / difficulty
-    if potion_health % difficulty != 0:
-        remove = potion_health % difficulty
-        new_health = new_health-remove
-        final_health = new_health + health
-    else:
-        remove = potion_health % difficulty
-        new_health = new_health-remove
-        final_health = new_health + health
+    new_health = int(new_health)
+    
+    final_health = new_health + health
+    
     print("Player Health = {}".format(final_health))
