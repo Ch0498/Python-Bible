cca22ae202564667b5e4d68289102aa16fb66850
Navdeep Kante <65970997+smartnavdeep@users.noreply.github.com>
2020-06-24 17:15:48 +0530

    Update slicer.py

--- slicer.py
+++ slicer.py
@@ -1,12 +1,12 @@
 from time import sleep
 
 while True:
-email = input("What is your email address?: ").strip()
-# slice out user name
-user = email[:email.index("@")]
-# slice domain name
-domain = email[email.index("@") + 1 :]
-# format message
-output = "Your username is {} and your domain name is {}".format(user, domain)
-# display output message 
-print(output)
+    email = input("What is your email address?: ").strip()
+    # slice out user name
+    user = email[:email.index("@")]
+    # slice domain name
+    domain = email[email.index("@") + 1 :]
+    # format message
+    output = "Your username is {} and your domain name is {}".format(user, domain)
+    # display output message 
+    print(output)
