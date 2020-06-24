a5525a12a34d4448d7da8f830f05c3d9d6a29549
Navdeep Kante <65970997+smartnavdeep@users.noreply.github.com>
2020-06-24 17:17:17 +0530

    Update slicer.py

--- slicer.py
+++ slicer.py
@@ -2,6 +2,7 @@ from time import sleep
 
 while True:
     email = input("What is your email address?: ").strip()
+    sleep(0.5)
     # slice out user name
     user = email[:email.index("@")]
     # slice domain name
