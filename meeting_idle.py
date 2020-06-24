2fc9aa831416a7b0d1841b76df7f7dc45ef09e15
Navdeep Kante <65970997+smartnavdeep@users.noreply.github.com>
2020-06-24 17:10:34 +0530

    Update meeting_idle.py

--- meeting_idle.py
+++ meeting_idle.py
@@ -9,7 +9,10 @@ res1 = "{} is greater than {}".format(num1,num2)
 res2 = "{} is greater than {}".format(num2,num1)
 if num1 > num2:
     print(res1)
+    sleep(0.5)
 elif num1 < num2:
     print(res2)
+    sleep(0.5)
 else: 
     print("Both are equal.")
+    sleep(0.5)
