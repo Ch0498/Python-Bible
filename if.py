11d5c2b83e59424c9eaff865078b47be0ae32654
Navdeep Kante <65970997+smartnavdeep@users.noreply.github.com>
2020-06-26 19:08:42 +0530

    Update if.py

--- if.py
+++ if.py
@@ -1,13 +1,17 @@
-print ("                                                  COMPARER")
-print ("                                                                                               by Navdeep")
-num1 = input("Enter the first number :")
-num2 = input("Enter the second number :")
-res1 = "{} is greater than {}".format(num1,num2)
-res2 = "{} is greater than {}".format(num2,num1)
-if num1 > num2:
-    print(res1)
-elif num1 < num2:
-    print(res2)
-else: 
-    print("Both are equal.")
-l = input(" ")
+from time import sleep
+
+while True:
+    num1 = input("Enter the first number :")
+    sleep(0.5)
+    num2 = input("Enter the second number :")
+    res1 = "{} is greater than {}".format(num1,num2)
+    res2 = "{} is greater than {}".format(num2,num1)
+    if num1 > num2:
+        print(res1)
+        sleep(0.5)
+    elif num1 < num2:
+        print(res2)
+        sleep(0.5)
+    else: 
+        print("Both are equal.")
+        sleep(0.5)
